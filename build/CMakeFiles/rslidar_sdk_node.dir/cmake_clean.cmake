1c3d8daea8062032d86d75a1bacb18762c9ba279
philtell <2267507789@qq.com>
2021-03-06 17:24:24 +0800

    first commit

---
+++ build/CMakeFiles/rslidar_sdk_node.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rslidar_sdk_node.dir/node/rslidar_sdk_node.cpp.o"
+  "CMakeFiles/rslidar_sdk_node.dir/src/msg/proto_msg/Proto_msg.LidarPoints.pb.cc.o"
+  "CMakeFiles/rslidar_sdk_node.dir/src/msg/proto_msg/Proto_msg.LidarScan.pb.cc.o"
+  "CMakeFiles/rslidar_sdk_node.dir/src/msg/proto_msg/Proto_msg.LidarPacket.pb.cc.o"
+  "rslidar_sdk_node.pdb"
+  "rslidar_sdk_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rslidar_sdk_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
